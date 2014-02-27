6f74542bcd5975987d8d5c7209c8a7bd2e89f91e
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2014-02-27 18:02:15 +0100

    switched to scheduled executor service for stop criterion checker

--- james/james-core/src/main/java/org/jamesframework/core/search/stopcriteria/StopCriterionChecker.java
+++ james/james-core/src/main/java/org/jamesframework/core/search/stopcriteria/StopCriterionChecker.java
@@ -15,8 +15,10 @@ package org.jamesframework.core.search.stopcriteria;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import org.jamesframework.core.exceptions.IncompatibleStopCriterionException;
 import org.jamesframework.core.search.Search;
@@ -26,24 +28,37 @@ import org.slf4j.LoggerFactory;
 /**
  * A stop criterion checker is responsible for checking the stop criteria of a given search, while this search is running. At construction,
  * a reference to the search is given. Stop criteria can be added using {@link #add(StopCriterion)}, which usually only happens from within
- * the search, which passes all its stop criteria to its dedicated checker. When {@link #startChecking()} is called, a timer thread will be
- * initiated to periodically check the stop criteria in the background, by default with a period of 1 second, while the search is running.
- * As soon as some stop criterion is satisfied, the checker will request the search to stop and will also terminate itself, deactivating the
- * timer thread.
+ * the search, which passes all its stop criteria to its dedicated checker after verifying compatibility.
+ * <p>
+ * When {@link #startChecking()} is called, a task will be scheduled to periodically check the stop criteria in the background, by default
+ * with a period of 1 second, while the search is running. These tasks are executed by a dedicated thread which is shared among all stop
+ * criterion checker instances, as checking stop criteria is not an intensive task.
+ * <p>
+ * As soon as some stop criterion is satisfied, the checker will request the search to stop and will also cancel the scheduled task
+ * for further execution.
  *
  * @author Herman De Beukelaer <herman.debeukelaer@ugent.be>
  */
 public class StopCriterionChecker {
 
+    // scheduled executor service, shared by all checkers
+    // (single thread as stop criterion checking is not intensive)
+    private static final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor(new ThreadFactory() {
+        @Override
+        public Thread newThread(Runnable r) {
+            return new Thread(r, "stop-crit-checker");
+        }
+    });
+    
+    // future of running scheduled task, null if not running
+    private ScheduledFuture runningTask;
+    
     // logger
     private static final Logger logger = LoggerFactory.getLogger(StopCriterionChecker.class);
 
     // period between consecutive checks, and corresponding time unit
     private long period;
     private TimeUnit periodTimeUnit;
-
-    // timer used for periodical checks (replaced with a new timer for every run)
-    private Timer timer;
 
     // search for which the stop criteria have to be checked
     private final Search<?> search;
@@ -65,6 +80,8 @@ public class StopCriterionChecker {
         // set default period to 1 second
         period = 1;
         periodTimeUnit = TimeUnit.SECONDS;
+        // not active
+        runningTask = null;
     }
 
     /**
@@ -88,8 +105,7 @@ public class StopCriterionChecker {
 
     /**
      * Set the period between consecutive stop criterion checks. By default, this period is set to 1 second. The new settings will apply as
-     * from the next call of {@link #startChecking()}. The period should be at least 1 millisecond, else an exception will be thrown when
-     * trying to start the timer.
+     * from the next call of {@link #startChecking()}.
      *
      * @param period period between two checks
      * @param timeUnit time unit of this period
@@ -100,40 +116,38 @@ public class StopCriterionChecker {
     }
 
     /**
-     * Start checking the stop criteria, in a timer thread fired to run in the background. If no stop criteria have been added, calling this
-     * method does not have any effect.
+     * Start checking the stop criteria, in a separate background thread. If the stop criterion checker is already active,
+     * or if no stop criteria have been added, calling this method does not have any effect.
      */
     public synchronized void startChecking() {
-        // check if some stop criteria were added
-        if (timer == null && !stopCriteria.isEmpty()) {
-            // create new timer (any previous one has been cancelled and cannot be reused)
-            timer = new Timer();
+        // check if not active and some stop criteria were added
+        if (runningTask == null && !stopCriteria.isEmpty()) {
             // schedule periodical check (starting without any delay)
-            timer.schedule(new StopCriterionCheckTimerTask(), 0, periodTimeUnit.toMillis(period));
+            runningTask = scheduler.scheduleWithFixedDelay(new StopCriterionCheckTask(), 0, period, periodTimeUnit);
             // log
             logger.debug("Stop criterion checker for search {} activated", search);
         }
     }
 
     /**
-     * Instructs the stop criterion checker to stop checking. In case the checker is active, this will terminate the timer thread which is
-     * running in the background, else, calling this method does not have any effect.
+     * Instructs the stop criterion checker to stop checking. In case the checker is not active,
+     * calling this method does not have any effect.
      */
     public synchronized void stopChecking() {
-        if (timer != null) {
-            // cancel timer
-            timer.cancel();
+        if (runningTask != null) {
+            // cancel task (let it complete its current run if running)
+            runningTask.cancel(false);
             // log
             logger.debug("Stop criterion checker for search {} deactivated", search);
-            // discard timer
-            timer = null;
+            // discard task
+            runningTask = null;
         }
     }
 
     /**
-     * Timer task used for checking the stop criteria.
+     * Runnable submitted to scheduler for checking the stop criteria.
      */
-    private class StopCriterionCheckTimerTask extends TimerTask {
+    private class StopCriterionCheckTask implements Runnable {
 
         // only warn once (per run) in case of incompatible stop criterion
         private boolean warned = false;
@@ -143,16 +157,16 @@ public class StopCriterionChecker {
          */
         @Override
         public void run() {
-            // check every stop criterion, until one is found to be satisfied or all have been checked
             boolean stopSearch = false;
             int i = 0;
+            // check every stop criterion, until one is found to be satisfied or all have been checked
             while (!stopSearch && i < stopCriteria.size()) {
                 try {
                     stopSearch = stopCriteria.get(i).searchShouldStop(search);
                 } catch (IncompatibleStopCriterionException ex){
                     // incompatible stop criterion; usually should not happen as search verifies compatibility when adding stop criteria
                     if(!warned){
-                        // issue a single warning (per run)
+                        // issue a single warning (per run, i.e. once during the lifetime of this specific check task)
                         logger.warn("Stop criterion checker for " + search + " contains an incompatible stop criterion", ex);
                         warned = true;
                     }
