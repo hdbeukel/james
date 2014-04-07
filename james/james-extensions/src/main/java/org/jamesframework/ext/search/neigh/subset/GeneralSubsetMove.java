40859ad8e6fe4edd41112f08cbc18302d418fbf7
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2014-04-07 10:28:20 +0200

    implemented and tested general subset move

---
+++ james/james-extensions/src/main/java/org/jamesframework/ext/search/neigh/subset/GeneralSubsetMove.java
@@ -0,0 +1,143 @@
+//  Copyright 2014 Herman De Beukelaer
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package org.jamesframework.ext.search.neigh.subset;
+
+import java.util.Collections;
+import java.util.Set;
+import org.jamesframework.core.exceptions.SolutionModificationException;
+import org.jamesframework.core.problems.solutions.SubsetSolution;
+import org.jamesframework.core.search.neigh.subset.SubsetMove;
+
+/**
+ * A general subset move adds and/or removes several IDs at once to/from the current selection
+ * of a subset solution.
+ * 
+ * @author <a href="mailto:herman.debeukelaer@ugent.be">Herman De Beukelaer</a>
+ */
+public class GeneralSubsetMove implements SubsetMove {
+
+    // set of IDs to add and remove
+    private Set<Integer> add;
+    private Set<Integer> delete;
+    
+    /**
+     * Creates a general subset move, indicating which IDs are to be added to and removed from
+     * the current selection when being applied to a given subset solution. Note that the sets
+     * <code>add</code> and <code>remove</code> are <b>not</b> copied: a reference to the given
+     * sets is stored in the subset move. If any of both sets is <code>null</code>, it will be
+     * replaced with an empty set.
+     * 
+     * @param add set of IDs to add to the selection
+     * @param remove set of IDs to remove from the selection
+     */
+    public GeneralSubsetMove(Set<Integer> add, Set<Integer> remove){
+        // store sets (set empty set in case of nullpointers)
+        if(add != null){
+            this.add = add;
+        } else {
+            this.add = Collections.emptySet();
+        }
+        if(remove != null){
+            this.delete = remove;
+        } else {
+            this.delete = Collections.emptySet();
+        }
+    }
+    
+    /**
+     * Returns an unmodifiable view of the set of IDs added to the selection when applying
+     * this move to a subset solution. The returned set may be empty.
+     * 
+     * @return set of added IDs
+     */
+    @Override
+    public Set<Integer> getAddedIDs() {
+        return Collections.unmodifiableSet(add);
+    }
+
+    /**
+     * Returns an unmodifiable view of the set of IDs removed from the selection when applying
+     * this move to a subset solution. The returned set may be empty.
+     * 
+     * @return set of removed IDs
+     */
+    @Override
+    public Set<Integer> getDeletedIDs() {
+        return Collections.unmodifiableSet(delete);
+    }
+
+    /**
+     * Returns the number of added IDs. May be zero.
+     * 
+     * @return number of added IDs
+     */
+    @Override
+    public int getNumAdded() {
+        return add.size();
+    }
+
+    /**
+     * Returns the number of removed IDs. May be zero.
+     * 
+     * @return number of removed IDs
+     */
+    @Override
+    public int getNumDeleted() {
+        return delete.size();
+    }
+
+    /**
+     * Apply this move to a given subset solution. The move can only be applied to a solution
+     * for which none of the added IDs are currently already selected and none of the removed
+     * IDs are currently not selected. This guarantees that calling {@link #undo(SubsetSolution)}
+     * will correctly undo the move.
+     * 
+     * @throws SolutionModificationException if some added ID is already selected, some removed ID is currently
+     *                                       not selected, or any ID does not correspond to an underlying entity
+     * @param solution solution to which to move will be applied
+     */
+    @Override
+    public void apply(SubsetSolution solution) {
+        // add IDs
+        for(int ID : add){
+            if(!solution.select(ID)){
+                throw new SolutionModificationException("Cannot add ID " + ID + " to selection (already selected).", solution);
+            }
+        }
+        // remove IDs
+        for(int ID : delete){
+            if(!solution.deselect(ID)){
+                throw new SolutionModificationException("Cannot remove ID " + ID + " from selection (currently not selected).", solution);
+            }
+        }
+    }
+
+    /**
+     * Undo this move after it has been successfully applied to the given subset solution,
+     * by removing all added IDs and adding all removed IDs. If the subset solution has been
+     * modified since successful application of this move, the result of this operation is
+     * undefined.
+     * 
+     * @param solution solution to which the move has been successfully applied
+     */
+    @Override
+    public void undo(SubsetSolution solution) {
+        // remove all added IDs
+        solution.deselectAll(add);
+        // add all removed IDs
+        solution.selectAll(delete);
+    }
+
+}
---
+++ james/james-extensions/src/test/java/org/jamesframework/ext/search/neigh/subset/GeneralSubsetMoveTest.java
@@ -0,0 +1,205 @@
+//  Copyright 2014 Herman De Beukelaer
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package org.jamesframework.ext.search.neigh.subset;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import org.jamesframework.core.problems.solutions.SubsetSolution;
+import org.jamesframework.core.util.SetUtilities;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+
+/**
+ * Test general subset move.
+ * 
+ * @author <a href="mailto:herman.debeukelaer@ugent.be">Herman De Beukelaer</a>
+ */
+public class GeneralSubsetMoveTest {
+
+    // random generator
+    private static final Random RG = new Random();
+    
+    // IDs
+    private static Set<Integer> IDs;
+    private static final int NUM_IDS = 100;
+    
+    /**
+     * Create data.
+     */
+    @BeforeClass
+    public static void setUpClass() {
+        // create set of all IDs
+        IDs = new HashSet<>();
+        for(int i=0; i<NUM_IDS; i++){
+            IDs.add(i);
+        }
+    }
+    
+    /**
+     * Test of getAddedIDs method, of class GeneralSubsetMove.
+     */
+    @Test
+    public void testGetAddedIDs() {
+        
+        System.out.println(" - test getAddedIDs");
+        
+        for(int i=0; i<100; i++){
+            // pick random number of IDs to add
+            int numadd = RG.nextInt(NUM_IDS);
+            // pick random set of IDs
+            Set<Integer> add = SetUtilities.getRandomSubset(IDs, numadd, RG);
+            GeneralSubsetMove move = new GeneralSubsetMove(add, null);
+            // verify
+            assertEquals(add, move.getAddedIDs());
+            assertEquals(numadd, move.getAddedIDs().size());
+        }
+        
+    }
+
+    /**
+     * Test of getDeletedIDs method, of class GeneralSubsetMove.
+     */
+    @Test
+    public void testGetDeletedIDs() {
+        
+        System.out.println(" - test getDeletedIDs");
+        
+        for(int i=0; i<100; i++){
+            // pick random number of IDs to delete
+            int numdel = RG.nextInt(NUM_IDS);
+            // pick random set of IDs
+            Set<Integer> delete = SetUtilities.getRandomSubset(IDs, numdel, RG);
+            GeneralSubsetMove move = new GeneralSubsetMove(null, delete);
+            // verify
+            assertEquals(delete, move.getDeletedIDs());
+            assertEquals(numdel, move.getDeletedIDs().size());
+        }
+        
+    }
+
+    /**
+     * Test of getNumAdded method, of class GeneralSubsetMove.
+     */
+    @Test
+    public void testGetNumAdded() {
+        
+        System.out.println(" - test getNumAdded");
+        
+        for(int i=0; i<100; i++){
+            // pick random number of IDs to add
+            int numadd = RG.nextInt(NUM_IDS);
+            // pick random set of IDs
+            Set<Integer> add = SetUtilities.getRandomSubset(IDs, numadd, RG);
+            GeneralSubsetMove move = new GeneralSubsetMove(add, null);
+            // verify
+            assertEquals(numadd, move.getNumAdded());
+        }
+        
+    }
+
+    /**
+     * Test of getNumDeleted method, of class GeneralSubsetMove.
+     */
+    @Test
+    public void testGetNumDeleted() {
+        
+        System.out.println(" - test getNumDeleted");
+        
+        for(int i=0; i<100; i++){
+            // pick random number of IDs to delete
+            int numdel = RG.nextInt(NUM_IDS);
+            // pick random set of IDs
+            Set<Integer> delete = SetUtilities.getRandomSubset(IDs, numdel, RG);
+            GeneralSubsetMove move = new GeneralSubsetMove(null, delete);
+            // verify
+            assertEquals(numdel, move.getNumDeleted());
+        }
+        
+    }
+
+    /**
+     * Test of apply method, of class GeneralSubsetMove.
+     */
+    @Test
+    public void testApply() {
+        
+        System.out.println(" - test apply");
+        
+        // create random subset solution (50%) selected
+        SubsetSolution sol = new SubsetSolution(IDs);
+        sol.selectAll(SetUtilities.getRandomSubset(IDs, NUM_IDS/2, RG));
+        
+        for(int i=0; i<100; i++){
+            // pick random number of IDs to add and remove
+            int numadd = RG.nextInt(sol.getNumUnselectedIDs());
+            int numdel = RG.nextInt(sol.getNumSelectedIDs());
+            // pick random set of IDs to add and remove
+            Set<Integer> add = SetUtilities.getRandomSubset(sol.getUnselectedIDs(), numadd, RG);
+            Set<Integer> delete = SetUtilities.getRandomSubset(sol.getSelectedIDs(), numdel, RG);
+            // create move
+            GeneralSubsetMove move = new GeneralSubsetMove(add, delete);
+            // apply move
+            move.apply(sol);
+            // verify
+            assertTrue(sol.getSelectedIDs().containsAll(add));
+            assertTrue(sol.getUnselectedIDs().containsAll(delete));
+        }
+        
+    }
+
+    /**
+     * Test of undo method, of class GeneralSubsetMove.
+     */
+    @Test
+    public void testUndo() {
+        
+        System.out.println(" - test undo");
+        
+        // create random subset solution (50%) selected
+        SubsetSolution sol = new SubsetSolution(IDs);
+        sol.selectAll(SetUtilities.getRandomSubset(IDs, NUM_IDS/2, RG));
+        
+        // create backup
+        SubsetSolution backup = new SubsetSolution(IDs);
+        backup.selectAll(sol.getSelectedIDs());
+        
+        for(int i=0; i<100; i++){
+            // pick random number of IDs to add and remove
+            int numadd = RG.nextInt(sol.getNumUnselectedIDs());
+            int numdel = RG.nextInt(sol.getNumSelectedIDs());
+            // pick random set of IDs to add and remove
+            Set<Integer> add = SetUtilities.getRandomSubset(sol.getUnselectedIDs(), numadd, RG);
+            Set<Integer> delete = SetUtilities.getRandomSubset(sol.getSelectedIDs(), numdel, RG);
+            // create move
+            GeneralSubsetMove move = new GeneralSubsetMove(add, delete);
+            // apply move
+            move.apply(sol);
+            // verify
+            assertTrue(sol.getSelectedIDs().containsAll(add));
+            assertTrue(sol.getUnselectedIDs().containsAll(delete));
+            // undo move
+            move.undo(sol);
+            // verify
+            assertEquals(backup, sol);
+            assertTrue(sol.getUnselectedIDs().containsAll(add));
+            assertTrue(sol.getSelectedIDs().containsAll(delete));
+        }
+        
+    }
+
+}
