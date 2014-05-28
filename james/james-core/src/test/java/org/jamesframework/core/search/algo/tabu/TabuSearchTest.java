508506b635c59d6e33092d1894cfd7400235f386
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2014-05-28 15:24:04 +0200

    tested tabu search

---
+++ james/james-core/src/test/java/org/jamesframework/core/search/algo/tabu/TabuSearchTest.java
@@ -0,0 +1,205 @@
+//  Copyright 2014 Herman De Beukelaer
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package org.jamesframework.core.search.algo.tabu;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.jamesframework.core.problems.solutions.SubsetSolution;
+import org.jamesframework.core.search.SearchTestTemplate;
+import org.jamesframework.test.stubs.NeverSatisfiedConstraintStub;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ * Test tabu search.
+ * 
+ * @author <a href="mailto:herman.debeukelaer@ugent.be">Herman De Beukelaer</a>
+ */
+@RunWith(Parameterized.class)
+public class TabuSearchTest extends SearchTestTemplate {
+
+    public interface TabuMemoryFactory {
+        public TabuMemory<SubsetSolution> createTabuMemory();
+    }
+    
+    // repeat all tests with different tabu memories
+    @Parameterized.Parameters
+    public static List<Object[]> data(){
+        List<Object[]> params = new ArrayList<>();
+        // first run: full tabu memory of size 10000
+        Object[] run1 = new Object[2];
+        run1[0] = "Full Tabu Memory (10000)";
+        run1[1] = new TabuMemoryFactory() {
+            @Override
+            public TabuMemory<SubsetSolution> createTabuMemory() {
+                return new FullTabuMemory<>(10000);
+            }
+        };
+        params.add(run1);
+        // second run: ID based tabu memory of size 20
+        Object[] run2 = new Object[2];
+        run2[0] = "ID Based Tabu Memory (20)";
+        run2[1] = new TabuMemoryFactory() {
+            @Override
+            public TabuMemory<SubsetSolution> createTabuMemory() {
+                return new IDBasedSubsetTabuMemory(20);
+            }
+        };
+        params.add(run2);
+        // return params
+        return params;
+    }
+    
+    public TabuSearchTest(String tabuMemoryDesc, TabuMemoryFactory tabuMemoryFactory){
+        this.tabuMemoryDesc = tabuMemoryDesc;
+        this.tabuMemoryFactory = tabuMemoryFactory;
+    }
+    
+    // tabu memory
+    private final TabuMemoryFactory tabuMemoryFactory;
+    private final String tabuMemoryDesc;
+    
+    // tabu search
+    private TabuSearch<SubsetSolution> search;
+    
+    // maximum runtime
+    private final long SINGLE_RUN_RUNTIME = 1000;
+    private final long MULTI_RUN_RUNTIME = 100;
+    private final TimeUnit MAX_RUNTIME_TIME_UNIT = TimeUnit.MILLISECONDS;
+    
+    // number of runs in multi run tests
+    private final int NUM_RUNS = 5;
+    
+    /**
+     * Print message when starting tests.
+     */
+    @BeforeClass
+    public static void setUpClass() {
+        System.out.println("# Testing TabuSearch ...");
+        SearchTestTemplate.setUpClass();
+    }
+
+    /**
+     * Print message when tests are complete.
+     */
+    @AfterClass
+    public static void tearDownClass() {
+        System.out.println("# Done testing TabuSearch!");
+    }
+    
+    @Override
+    @Before
+    public void setUp(){
+        // call super
+        super.setUp();
+        // create tabu search
+        search = new TabuSearch<>(problem, neigh, tabuMemoryFactory.createTabuMemory());
+        // print description of applied tabu memory
+        System.out.println(" - TABU MEMORY: " + tabuMemoryDesc);
+    }
+    
+    @After
+    public void tearDown(){
+        // dispose search
+        search.dispose();
+    }
+
+    /**
+     * Test single run.
+     */
+    @Test
+    public void testSingleRun() {
+        System.out.println(" - test single run");
+        // single run
+        singleRunWithMaxRuntime(search, problem, SINGLE_RUN_RUNTIME, MAX_RUNTIME_TIME_UNIT);
+    }
+    
+    /**
+     * Test single run with unsatisfiable constraint.
+     */
+    @Test
+    public void testSingleRunWithUnsatisfiableConstraint() {
+        System.out.println(" - test single run with unsatisfiable constraint");
+        // add constraint
+        problem.addRejectingConstraint(new NeverSatisfiedConstraintStub());
+        // single run
+        singleRunWithMaxRuntime(search, problem, SINGLE_RUN_RUNTIME, MAX_RUNTIME_TIME_UNIT);
+        // verify
+        assertNull(search.getBestSolution());
+    }
+    
+    /**
+     * Test subsequent runs (maximizing).
+     */
+    @Test
+    public void testSubsequentRuns() {
+        System.out.println(" - test subsequent runs (maximizing)");
+        // perform multiple runs (maximizing objective)
+        multiRunWithMaximumRuntime(search, MULTI_RUN_RUNTIME, MAX_RUNTIME_TIME_UNIT, NUM_RUNS, true, true);
+    }
+    
+    /**
+     * Test subsequent runs (minimizing).
+     */
+    @Test
+    public void testSubsequentRunsMinimizing() {
+        System.out.println(" - test subsequent runs (minimizing)");
+        // set minimizing
+        obj.setMinimizing();
+        // perform multiple runs (maximizing objective)
+        multiRunWithMaximumRuntime(search, MULTI_RUN_RUNTIME, MAX_RUNTIME_TIME_UNIT, NUM_RUNS, false, true);
+    }
+    
+    /**
+     * Test subsequent runs with unsatisfiable constraint.
+     */
+    @Test
+    public void testSubsequentRunsWithUnsatisfiableConstraint() {
+        System.out.println(" - test subsequent runs with unsatisfiable constraint");
+        // set constraint
+        problem.addRejectingConstraint(new NeverSatisfiedConstraintStub());
+        // perform multiple runs (maximizing objective)
+        multiRunWithMaximumRuntime(search, MULTI_RUN_RUNTIME, MAX_RUNTIME_TIME_UNIT, NUM_RUNS, true, true);
+        // verify
+        assertNull(search.getBestSolution());
+    }
+    
+    /**
+     * Test subsequent runs with penalizing constraint.
+     */
+    @Test
+    public void testSubsequentRunsWithPenalizingConstraint() {
+        System.out.println(" - test subsequent runs with penalizing constraint");
+        // set constraint
+        problem.addPenalizingConstraint(constraint);
+        // perform 3 times as many runs as usual for this harder problem (maximizing objective)
+        multiRunWithMaximumRuntime(search, MULTI_RUN_RUNTIME, MAX_RUNTIME_TIME_UNIT, 3*NUM_RUNS, true, false);
+        System.out.println("   >>> best: " + search.getBestSolutionEvaluation());
+        // constraint satisfied ?
+        if(problem.getViolatedConstraints(search.getBestSolution()).isEmpty()){
+            System.out.println("   >>> constraint satisfied!");
+        } else {
+            System.out.println("   >>> constraint not satisfied, penalty " + constraint.computePenalty(search.getBestSolution(), data));
+        }
+    }
+
+}
