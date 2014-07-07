3207ef30912d0f4a92ba58db759b8009cd4d9e00
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2014-07-07 19:25:30 +0200

    compile bootstrap from source

---
+++ _bootstrap/node_modules/grunt-jscs-checker/node_modules/jscs/lib/cli.js
@@ -0,0 +1,126 @@
+/**
+ * Command line implementation for JSCS.
+ *
+ * Common usage case is:
+ *
+ * ./node_modules/.bin/jscs file1 dir1 file2 dir2
+ */
+var Checker = require('./checker');
+var configFile = require('./cli-config');
+var preset = require('./options/preset');
+
+var Vow = require('vow');
+
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(program) {
+    var reporterPath, reporter, config;
+    var defer = Vow.defer();
+    var promise = defer.promise();
+    var checker = new Checker();
+    var args = program.args;
+    var returnArgs = {
+        checker: checker,
+        reporter: program.reporter,
+        promise: promise
+    };
+
+    promise.always(function(status) {
+        process.exit(status.valueOf());
+    });
+
+    try {
+        config = configFile.load(program.config);
+    } catch (e) {
+        console.error('Config source is corrupted -', e.toString());
+        defer.reject(1);
+
+        return returnArgs;
+    }
+
+    /**
+     * Trying to load config.
+     * Custom config path can be specified using '-c' option.
+     */
+    if (!config && !program.preset) {
+        if (program.config) {
+            console.error('Configuration source', program.config, 'was not found.');
+        } else {
+            console.error('No configuration found. Add a .jscsrc file to your project root or use the -c option.');
+        }
+
+        defer.reject(1);
+
+        return returnArgs;
+    }
+
+    if (program.preset && !preset.exists(program.preset)) {
+        console.error(preset.getDoesNotExistError(program.preset));
+        defer.reject(1);
+
+        return returnArgs;
+    }
+
+    if (args.length === 0) {
+        console.error('No input files specified. Try option --help for usage information.');
+        defer.reject(1);
+
+        return returnArgs;
+    }
+
+    if (!config) {
+        config = {};
+    }
+
+    if (program.preset) {
+        config.preset = program.preset;
+    }
+
+    if (program.reporter) {
+        reporterPath = path.resolve(process.cwd(), program.reporter);
+        returnArgs.reporter = reporterPath;
+
+        if (!fs.existsSync(reporterPath)) {
+            reporterPath = './reporters/' + program.reporter;
+        }
+
+    } else {
+        reporterPath = './reporters/' + (program.colors ? 'console' : 'text');
+    }
+
+    try {
+        reporter = require(reporterPath);
+
+    } catch (e) {
+        console.error('Reporter "%s" doesn\'t exist.', reporterPath);
+        defer.reject(1);
+
+        return returnArgs;
+    }
+
+    checker.registerDefaultRules();
+    checker.configure(config);
+
+    /**
+     * Processing specified files and dirs.
+     */
+    Vow.all(args.map(checker.checkPath, checker)).then(function(results) {
+        var errorsCollection = [].concat.apply([], results);
+
+        reporter(errorsCollection);
+
+        errorsCollection.forEach(function(errors) {
+            if (!errors.isEmpty()) {
+                defer.reject(2);
+            }
+        });
+
+        defer.resolve(0);
+    }).fail(function(e) {
+        console.error(e.stack);
+        defer.reject(1);
+    });
+
+    return returnArgs;
+};
---
+++ _bootstrap/node_modules/grunt-jscs-checker/node_modules/jscs/lib/options/preset.js
@@ -0,0 +1,55 @@
+var presets = {
+    // https://contribute.jquery.org/style-guide/js/
+    jquery: require('../../presets/jquery.json'),
+    // https://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml
+    google: require('../../presets/google.json'),
+    // https://github.com/ymaps/codestyle/blob/master/js.md
+    yandex: require('../../presets/yandex.json'),
+    // https://www.mediawiki.org/wiki/Manual:Coding_conventions/JavaScript
+    wikimedia: require('../../presets/wikimedia.json')
+};
+
+module.exports = {
+    /**
+     * Get does not exist error
+     * @param {String} preset
+     * @return {String}
+     */
+    getDoesNotExistError: function(preset) {
+        return 'Preset "' + preset + '" does not exist';
+    },
+    /**
+     * Is preset exists in jscs
+     * @param {String} preset
+     * @return {Boolean}
+     */
+    exists: function(preset) {
+        return !!presets[preset];
+    },
+
+    /**
+     * Extend jscs config with preset rules
+     * @param {Object} config
+     * @return {Boolean}
+     */
+    extend: function(config) {
+        if (!config.preset) {
+            return true;
+        }
+
+        var preset = presets[config.preset];
+
+        if (!preset) {
+            return false;
+        }
+
+        delete config.preset;
+        for (var rule in preset) {
+            if (!(rule in config)) {
+                config[rule] = preset[rule];
+            }
+        }
+
+        return true;
+    }
+};
