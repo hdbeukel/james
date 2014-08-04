c161348a7d5ff04e857c4dd00a8e5ba4cebfd5c5
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2014-08-04 10:23:36 +0200

    updated license headers

--- james/james-core/src/main/java/org/jamesframework/core/search/algo/BasicParallelSearch.java
+++ james/james-core/src/main/java/org/jamesframework/core/search/algo/BasicParallelSearch.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Herman De Beukelaer
+ * Copyright 2014 Ghent University, Bayer CropScience.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.jamesframework.core.search.algo;
 
 import java.util.ArrayList;
--- james/james-core/src/main/java/org/jamesframework/core/search/algo/tabu/TabuMemory.java
+++ james/james-core/src/main/java/org/jamesframework/core/search/algo/tabu/TabuMemory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Herman De Beukelaer
+ * Copyright 2014 Ghent University, Bayer CropScience.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
