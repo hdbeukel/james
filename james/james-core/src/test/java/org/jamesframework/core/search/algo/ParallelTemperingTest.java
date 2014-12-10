04886b20c60a875ab8f600d63d0691073e3aedab
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2014-12-10 17:47:01 +0100

    extended parallel tempering tests

--- james/james-core/src/test/java/org/jamesframework/core/search/algo/ParallelTemperingTest.java
+++ james/james-core/src/test/java/org/jamesframework/core/search/algo/ParallelTemperingTest.java
@@ -16,10 +16,18 @@
 
 package org.jamesframework.core.search.algo;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
+import org.jamesframework.core.exceptions.SearchException;
 import org.jamesframework.core.problems.objectives.evaluations.PenalizedEvaluation;
 import org.jamesframework.core.subset.SubsetSolution;
 import org.jamesframework.core.search.SearchTestTemplate;
+import org.jamesframework.core.search.neigh.Neighbourhood;
+import org.jamesframework.core.search.status.SearchStatus;
+import org.jamesframework.core.subset.neigh.SingleSwapNeighbourhood;
 import org.jamesframework.test.stubs.NeverSatisfiedConstraintStub;
 import org.jamesframework.test.stubs.NeverSatisfiedPenalizingConstraintStub;
 import org.jamesframework.test.util.TestConstants;
@@ -88,12 +96,172 @@ public class ParallelTemperingTest extends SearchTestTemplate {
     @After
     public void tearDown(){
         // print number of accepted/rejected moves of last run
-        System.out.println("   >>> num accepted/rejected moves during last run: "
-                            + search.getNumAcceptedMoves() + "/" + search.getNumRejectedMoves());
+        if(search.getSteps() > 0){
+            System.out.println("   >>> num accepted/rejected moves during last run: "
+                                + search.getNumAcceptedMoves() + "/" + search.getNumRejectedMoves());
+        }
         // dispose search
-        search.dispose();
+        if(search.getStatus() == SearchStatus.IDLE){
+            search.dispose();
+        }
     }
 
+    /**
+     * Test exceptions trown from constructor.
+     */
+    @Test
+    public void testConstructorExceptions(){
+        
+        System.out.println(" - test constructor exceptions");
+        
+        boolean thrown;
+        
+        thrown = false;
+        try{
+            search = new ParallelTempering<>(problem, neigh, 0, MIN_TEMP, MAX_TEMP);
+        } catch(IllegalArgumentException ex){
+            thrown = true;
+        }
+        assertTrue(thrown);
+        
+        thrown = false;
+        try{
+            search = new ParallelTempering<>(problem, neigh, numReplicas, 0.0, MAX_TEMP);
+        } catch(IllegalArgumentException ex){
+            thrown = true;
+        }
+        assertTrue(thrown);
+        
+        thrown = false;
+        try{
+            search = new ParallelTempering<>(problem, neigh, numReplicas, MIN_TEMP, 0.0);
+        } catch(IllegalArgumentException ex){
+            thrown = true;
+        }
+        assertTrue(thrown);
+        
+        thrown = false;
+        try{
+            search = new ParallelTempering<>(problem, neigh, numReplicas, MAX_TEMP, MIN_TEMP);
+        } catch(IllegalArgumentException ex){
+            thrown = true;
+        }
+        assertTrue(thrown);
+        
+    }
+    
+    @Test
+    public void testSetReplicaSteps() {
+        System.out.println(" - test set replica steps");
+        
+        boolean thrown = false;
+        try{
+            search.setReplicaSteps(0);
+        } catch (IllegalArgumentException ex){
+            thrown = true;
+        }
+        assertTrue(thrown);
+        
+        thrown = false;
+        try{
+            search.setReplicaSteps(-1);
+        } catch (IllegalArgumentException ex){
+            thrown = true;
+        }
+        assertTrue(thrown);
+        
+        for(int i=0; i<100; i++){
+            int repSteps = RG.nextInt(500)+1;
+            search.setReplicaSteps(repSteps);
+            assertEquals(repSteps, search.getReplicaSteps());
+        }
+    }
+    
+    @Test
+    public void testGetReplicas(){
+        System.out.println(" - test get replicas");
+        
+        List<MetropolisSearch<SubsetSolution>> reps = search.getReplicas();
+        
+        assertEquals(numReplicas, reps.size());
+        
+        reps.forEach(rep -> {
+            assertEquals(problem, rep.getProblem());
+            assertEquals(neigh, rep.getNeighbourhood());
+            assertTrue(rep.getTemperature() >= MIN_TEMP && rep.getTemperature() <= MAX_TEMP);
+        });
+    }
+    
+    @Test
+    public void testSetNeighbourhood(){
+        System.out.println(" - test set neighbourhood");
+        
+        Neighbourhood<SubsetSolution> newNeigh = new SingleSwapNeighbourhood();
+        
+        search.setNeighbourhood(newNeigh);
+        assertEquals(newNeigh, search.getNeighbourhood());
+        
+        search.getReplicas().forEach(rep -> {
+            assertEquals(newNeigh, rep.getNeighbourhood());
+        });
+    }
+    
+    @Test
+    public void testSetCurrentSolution(){
+        System.out.println(" - test set current solution");
+        
+        SubsetSolution sol = problem.createRandomSolution();
+        
+        search.setCurrentSolution(sol);
+        assertEquals(sol, search.getCurrentSolution());
+        
+        search.getReplicas().forEach(rep -> {
+            assertNotSame(sol, rep.getCurrentSolution());
+            assertEquals(sol, rep.getCurrentSolution());
+        });
+    }
+    
+    @Test
+    public void testInterruptReplicaExecution(){
+        System.out.println(" - test interrupt replica execution");
+        
+        // schedule task to interrupt the thread that runs the main search
+        // while it is waiting for the replicas to complete their current run
+        final Thread thr = Thread.currentThread();
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                thr.interrupt();
+            }
+        }, 500);
+        
+        boolean thrown = false;
+        try{
+            search.setReplicaSteps(1000000000); // keep those replicas busy!
+            search.start();
+        } catch(SearchException ex){
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+    
+    @Test
+    public void testCorreuptReplicas(){
+        System.out.println(" - test corrupt replicas");
+        
+        List<MetropolisSearch<SubsetSolution>> reps = search.getReplicas();
+        Collections.reverse(reps); // whoops! I just broke order of temperatures
+        
+        boolean thrown = false;
+        try{
+            search.start();
+        } catch(SearchException ex){
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+    
     /**
      * Test single run.
      */
